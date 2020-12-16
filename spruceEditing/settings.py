ad2837e41213310ec0f4d861f333ab54b38c6303
Couragyn <couragyn@gmail.com>
2020-12-16 02:50:20 +0100

    added static folder

--- spruceEditing/settings.py
+++ spruceEditing/settings.py
@@ -155,7 +155,7 @@ SECURE_HSTS_PRELOAD = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'static')
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = [
