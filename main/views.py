f2719a0f157442f35daddce88f95a11a9843ba4c
Couragyn <couragyn@gmail.com>
2020-12-30 01:56:47 +0100

    first run through of contact form

--- main/views.py
+++ main/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, render_to_response, get_object_or_404
+from django.core.mail import send_mail
 
-# Create your views here.
+def contact_page(request):
+    return render(request, "contact")
+
+def send_contact(request):
+    name = request.POST.get("name")
+    email = request.POST.get("email")
+    subject = request.POST.get("subject")
+    message = request.POST.get("message")
+ 
+    send_mail("New Message From the Contact Form", message, email, ["[ email you'll       like to send the message to ]"],
+	html_message="<html>You received a new message from the contact form."Name:" + name + "Email Address:" + email + "Subject:" + subject + "&lt;br/&gt;"Message:" + message + "</html>")
+	    request.session['sendmessage'] = "Message Has Been Sent"
+	 
+	return HttpResponseRedirect('[ your contact page url ]')
