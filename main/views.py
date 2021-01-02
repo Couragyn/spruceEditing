41df046a6ed71f3b78f61cec049ea9bb897d77bc
Couragyn <couragyn@gmail.com>
2021-01-02 02:15:19 +0100

    added fontawesome. Added blank quote page. Put in real text. Added heroimage

--- main/views.py
+++ main/views.py
@@ -4,10 +4,10 @@ from django.core.mail import send_mail
 
 
 def send_contact(request):
-	name = request.POST.get("name")
-	email = request.POST.get("email")
-	subject = request.POST.get("subject")
-	message = request.POST.get("message")
+	name = request.POST.get("nameInput")
+	email = request.POST.get("emailInput")
+	phone = request.POST.get("phoneInput")
+	message = request.POST.get("messageInput")
  
 	send_mail("New Contact Form Message", message, email, ["SpruceEditing@gmail.com"],
 	html_message="<html>You received a new message from the contact form. </ br> Name:" + name + "Email Address:" + email + "Subject:" + subject + "&lt;br/&gt;Message:" + message + "</html>")
--- spruceEditing/settings.py
+++ spruceEditing/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'django_summernote',
     'anymail',
+    'fontawesome-free',
 ]
 
 MIDDLEWARE = [
