e9d77fa8817cc8463f3b11622386ff2c9081b483
Couragyn <couragyn@gmail.com>
2021-01-02 06:15:23 +0100

    try again

--- main/views.py
+++ main/views.py
@@ -49,12 +49,12 @@ class contact_page_view(View):
 			try:
 				email_body = "<html>You received a new message from the contact form. </ br></ br> Name:" + name + "Email Address:" + email + "Phone:" + phone + "&lt;br/&gt;Message:" + message + "</html>"
 				mail = EmailMessage("New Contact Form Message", email_body, email, ["SpruceEditing@gmail.com"])
-				for f in files:
+				for f in attached:
 					mail.attach(f.name, f.read(), f.content_type)
 				mail.send()
 				return render(request, self.template_name, {'email_form': form, 'error_message': 'Email successfully sent to SpruceEditing@gmail.com'})
 			except:
-				return render(request, self.template_name, {'email_form': form, 'error_message': 'Either the attachment is too big or corrupt'})
+				return render(request, self.template_name, {'email_form': form, 'error_message': 'Error sending email. Please try again later. Please make sure attachements are under 10mb'})
 
 		return render(request, self.template_name, {'email_form': form, 'error_message': 'Unable to send email. Please try again later'})
 
