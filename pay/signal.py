ec0b8f94a1867343880f84e5d4d56ed5e9f4530f
Couragyn <couragyn@gmail.com>
2021-02-06 03:23:45 +0100

    didnt import pay in signal. ingored ngrok

--- pay/signal.py
+++ pay/signal.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from .models import Order
+from .models import Pay
 from paypal.standard.ipn.signals import valid_ipn_received
 from django.dispatch import receiver
 
@@ -7,6 +7,7 @@ from django.dispatch import receiver
 @receiver(valid_ipn_received)
 def payment_notification(sender, **kwargs):
     ipn = sender
+    print(ipn)
     if ipn.payment_status == 'Completed':
         # payment was successful
         order = get_object_or_404(Pay, id=ipn.invoice)
